6188d4cf316d84ddcf42f2145dc99b8698cfa468
shane lee <shanelee007@gmail.com>
2017-04-28 23:01:21 +1000

    bumping versions

--- jsConsumer/karma.conf.js
+++ jsConsumer/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '**/dist/pact.web.js',
+      '**/dist/pact-web.js',
       '**/*.pact.spec.js'
     ],
 
